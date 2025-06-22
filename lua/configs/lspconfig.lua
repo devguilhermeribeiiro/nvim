38bf30adee11319c9434a2771072ac44020c21ac
devgr <devguilhermeribeiro000@gmail.com>
2025-06-21 22:00:17 -0300

    Setup for others langs

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "intelephense" }
+local servers = {
+  "html", "cssls", "intelephense", 
+  "pyright", "jsdtls", "crystalline",
+  "nimlangserver", "nginx-language-server"
+}
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
