a8fd2052da04c24cee05a47bb7c0e628d10f16bb
Guilherme Ribeiro <devguilhermeribeiro000@proton.me>
2026-05-02 07:55:13 -0300

    Adding autocomplete

--- lua/plugins/rustacean-vim.lua
+++ lua/plugins/rustacean-vim.lua
@@ -6,4 +6,29 @@ return {
   -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
   -- No need for lazy.nvim to lazy-load it.
   lazy = false,
+
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(client, bufnr)
+          local opts = { buffer = bufnr }
+
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+            },
+            checkOnSave = {
+              command = "clippy"
+            },
+          },
+        },
+      },
+    }
+  end,
+
 }
